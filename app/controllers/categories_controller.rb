5a0f0536d9ccc2bea3525e3c7e0951f52059c811
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-05-11 02:35:09 -0700

    Created form to edit resources views/categories/edit.html.erb and added code in the back end to implement the changes controllers/categories_controller.rb Now I can Update other resources, the changes are reflected in the box since it pulls the info from the db

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,9 +16,18 @@ class CategoriesController < ApplicationController
   end
 
   def update
+    @category = Category.find(params[:id])
+
+    @category.update(category_params)
+
+    flash[:notice] = 'Category Updated'
+
+    redirect_to categories_path
+
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def destroy
